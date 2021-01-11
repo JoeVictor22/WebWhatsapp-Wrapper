d9f83039c3717e939e96d0e804fd30e869781bca
joel <joelvictor1746@gmail.com>
2021-01-11 08:42:50 -0300

    fixes no deply do docker

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ DEBUG = True
 
 
 
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123@localhost/chatbot"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123@postgres_docker:5432/chatbot"
 print(SQLALCHEMY_DATABASE_URI)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
